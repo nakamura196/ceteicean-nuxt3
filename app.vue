a45dddde26647111a560e664e5846b6a2afa4bfc
Satoru <you@example.com>
2023-07-27 21:09:35 +0900

    feat: initial commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import CETEI from "CETEIcean";
+
+const teiHTML = ref<string>("");
+
+var CETEIcean = new CETEI();
+CETEIcean.getHTML5(
+  "https://raw.githubusercontent.com/TEIC/CETEIcean/master/test/testTEI.xml",
+  function (data: HTMLElement) {
+    teiHTML.value = data.outerHTML;
+  }
+);
+</script>
+<template>
+  <div>
+    TEI would go here...
+    <div id="tei" v-html="teiHTML" />
+  </div>
+</template>
